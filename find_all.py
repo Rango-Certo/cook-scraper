1d6156a7e0a78568f21517ae6b4155b426dbd47e
Adriaon Righi <contato@adrianorighi.com>
2022-01-26 13:18:31 -0300

    chore: add package

--- find_all.py
+++ find_all.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
-connection = MongoClient('localhost', 27017)
+connection = MongoClient('mongodb+srv://rangocerto:rightfood@cluster0.tljy8.mongodb.net/rightFood?retryWrites=true&w=majority')
 database = connection.cooks
 collection = database.cooking_recipes
 
